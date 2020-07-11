62ca5be6302d686233132e37eb1f2181e23b7e0d
GautamKumar <25435568+gautamajay52@users.noreply.github.com>
2020-07-12 01:12:15 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -218,29 +218,32 @@ async def call_apropriate_function(
         response
     )
     LOGGER.info(final_response)
-    message_to_send = ""
-    for key_f_res_se in final_response:
-        local_file_name = key_f_res_se
-        message_id = final_response[key_f_res_se]
-        channel_id = str(sent_message_to_update_tg_p.chat.id)[4:]
-        private_link = f"https://t.me/c/{channel_id}/{message_id}"
-        message_to_send += "👉 <a href='"
-        message_to_send += private_link
-        message_to_send += "'>"
-        message_to_send += local_file_name
-        message_to_send += "</a>"
-        message_to_send += "\n"
-    if message_to_send != "":
-        mention_req_user = f"<a href='tg://user?id={user_id}'>Your Requested Files</a>\n\n"
-        message_to_send = mention_req_user + message_to_send
-        message_to_send = message_to_send + "\n\n" + "#uploads"
-    else:
-        message_to_send = "<i>FAILED</i> to upload files. 😞😞"
-    await user_message.reply_text(
-        text=message_to_send,
-        quote=True,
-        disable_web_page_preview=True
-    )
+    try:
+        message_to_send = ""
+        for key_f_res_se in final_response:
+            local_file_name = key_f_res_se
+            message_id = final_response[key_f_res_se]
+            channel_id = str(sent_message_to_update_tg_p.chat.id)[4:]
+            private_link = f"https://t.me/c/{channel_id}/{message_id}"
+            message_to_send += "👉 <a href='"
+            message_to_send += private_link
+            message_to_send += "'>"
+            message_to_send += local_file_name
+            message_to_send += "</a>"
+            message_to_send += "\n"
+        if message_to_send != "":
+            mention_req_user = f"<a href='tg://user?id={user_id}'>Your Requested Files</a>\n\n"
+            message_to_send = mention_req_user + message_to_send
+            message_to_send = message_to_send + "\n\n" + "#uploads"
+        else:
+            message_to_send = "<i>FAILED</i> to upload files. 😞😞"
+        await user_message.reply_text(
+            text=message_to_send,
+            quote=True,
+            disable_web_page_preview=True
+        )
+    except:
+        pass
     return True, None
 #
 
