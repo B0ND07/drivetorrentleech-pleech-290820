0a649dd592405c3f20b39dee8af3ea05843a1f07
Knight RiDer <44409027+Hashimkp@users.noreply.github.com>
2020-08-29 18:55:11 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -33,12 +33,12 @@ async def new_join_f(client, message):
 
 
 async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
+    # await message.reply_text("For Queries Contact @RiDerOO7", quote=True)
     #channel_id = str(AUTH_CHANNEL)[4:]
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""join this group forr help-- @torrentleechgdrivesupport\n\n And also don't forget to fork this repo: <a href="https://github.com/gautamajay52/TorrentLeech-Gdrive">TorrentLeech-Gdrive</a>""", disable_web_page_preview=True)
+    await message.reply_text("""Bot Created By-- @RiDerOO7\n\n And also don't Spam Me</a>""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
